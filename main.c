f6e4eec89a7c6966b5ae24081dcee5bcaec426e7
lucasreljic <lucas.reljic@gmail.com>
2022-11-02 16:08:24 -0400

    ev3 makes music now

--- main.c
+++ main.c
@@ -9,15 +9,121 @@ int rotateRobot(int angle);
 void drivePID(int distance);
 void driveBoth(int pwrL, int pwrR);
 void drive(int pwr);
+void MissionImpossible()
+{
+  //        100 = Tempo
+  //          6 = Default octave
+  //    Quarter = Default note length
+  //        10% = Break between notes
+  //
+  playTone(880,    7);
+  wait1Msec(  75);  // Note(D, Duration(32th))
+  playTone(  933,    7);
+  wait1Msec(  75);  // Note(D#, Duration(32th))
+  playTone(  880,    7);
+  wait1Msec(  75);  // Note(D, Duration(32th))
+  playTone(  933,    7);
+  wait1Msec(  75);  // Note(D#, Duration(32th))
+  playTone(  880,    7);
+  wait1Msec(  75);  // Note(D, Duration(32th))
+  playTone(  933,    7);
+  wait1Msec(  75);  // Note(D#, Duration(32th))
+  playTone(  880,    7);
+  wait1Msec(  75);  // Note(D, Duration(32th))
+  playTone(  933,    7);
+  wait1Msec(  75);  // Note(D#, Duration(32th))
+  playTone(  880,    7);
+  wait1Msec(  75);  // Note(D, Duration(32th))
+  playTone(  880,    7);
+  wait1Msec(  75);  // Note(D, Duration(32th))
+  playTone(  933,    7);
+  wait1Msec(  75);  // Note(D#, Duration(32th))
+  playTone(  988,    7);
+  wait1Msec(  75);  // Note(E, Duration(32th))
+  playTone( 1047,    7);
+  wait1Msec(  75);  // Note(F, Duration(32th))
+  playTone( 1109,    7);
+  wait1Msec(  75);  // Note(F#, Duration(32th))
+  playTone( 1175,    7); wait1Msec(  75);  // Note(G, Duration(32th))
+  playTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  playTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  playTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  playTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  playTone( 1398,   14); wait1Msec( 150);  // Note(A#, Duration(16th))
+  playTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  playTone(  784,   14); wait1Msec( 150);  // Note(C, Duration(16th))
+  playTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  playTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  playTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  PlayTone( 1047,   14); wait1Msec( 150);  // Note(F, Duration(16th))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone( 1109,   14); wait1Msec( 150);  // Note(F#, Duration(16th))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  PlayTone( 1398,   14); wait1Msec( 150);  // Note(A#, Duration(16th))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone(  784,   14); wait1Msec( 150);  // Note(C, Duration(16th))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(    0,   27); wait1Msec( 300);  // Note(Rest, Duration(Eighth))
+  PlayTone( 1047,   14); wait1Msec( 150);  // Note(F, Duration(16th))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone( 1109,   14); wait1Msec( 150);  // Note(F#, Duration(16th))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone( 1398,   14); wait1Msec( 150);  // Note(A#, Duration(16th))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(  880,  108); wait1Msec(1200);  // Note(D, Duration(Half))
+  PlayTone(    0,    7); wait1Msec(  75);  // Note(Rest, Duration(32th))
+  PlayTone( 1398,   14); wait1Msec( 150);  // Note(A#, Duration(16th))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(  831,  108); wait1Msec(1200);  // Note(C#, Duration(Half))
+  PlayTone(    0,    7); wait1Msec(  75);  // Note(Rest, Duration(32th))
+  PlayTone( 1398,   14); wait1Msec( 150);  // Note(A#, Duration(16th))
+  PlayTone( 1175,   14); wait1Msec( 150);  // Note(G, Duration(16th))
+  PlayTone(  784,  108); wait1Msec(1200);  // Note(C, Duration(Half))
+  PlayTone(    0,   14); wait1Msec( 150);  // Note(Rest, Duration(16th))
+  PlayTone(  932,   14); wait1Msec( 150);  // Note(A#5, Duration(16th))
+  PlayTone(  784,   14); wait1Msec( 150);  // Note(C, Duration(16th))
+  return;
+}
+
+void fifth()
+{
+  //         63 = Tempo
+  //          5 = Default octave
+  //    Quarter = Default note length
+  //        10% = Break between notes
+  //
+  PlayTone(  659,   43); wait1Msec( 476);  // Note(E, Duration(Eighth))
+  PlayTone(  880,   43); wait1Msec( 476);  // Note(A, Duration(Eighth))
+  PlayTone(  987,   43); wait1Msec( 476);  // Note(B, Duration(Eighth))
+  PlayTone(  783,   43); wait1Msec( 476);  // Note(G, Duration(Eighth))
+  PlayTone(  622,  171); wait1Msec(1905);  // Note(D#, Duration(Half))
+  return;
+}
+
+
+
+
+
 
 
 task main()
 {
+	fifth();
+	MissionImpossible();
 	clearDebugStream();
 	configureSensors();
-
 	while (true)
 	{
+
 		while(!getButtonPress(buttonAny))
 		{}
 
