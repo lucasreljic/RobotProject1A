1da44965980e38d8d321895d311763e7d7ce9602
lucasreljic <lucas.reljic@gmail.com>
2022-11-02 23:59:23 -0400

    initial commit

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 
 
-const int motorLeft = motorD;
-const int motorRight = motorA;
-
+const int MOTOR_LEFT = motorD;
+const int MOTOR_RIGHT = motorA;
+const int MAX_POWER = 70;
 
 void configureSensors();
 int rotateRobot(int angle);
 void drivePID(int distance);
+void correctiveDrive(int distance);
+
 void driveBoth(int pwrL, int pwrR);
 void drive(int pwr);
+int rotateAbsolute(int angle);
 
 
 task main()
@@ -22,13 +25,13 @@ task main()
 		{}
 
 		if (getButtonPress(buttonLeft))
-			rotateRobot(-90);
+			rotateAbsolute(-90);
 		else if (getButtonPress(buttonRight))
-			rotateRobot(90);
+			rotateAbsolute(90);
 		else if (getButtonPress(buttonUp))
-			drivePID(50);
+			correctiveDrive(-150);
 		else if (getButtonPress(buttonDown))
-			drivePID(-50);
+			correctiveDrive(150);
 		else if (getButtonPress(buttonEnter))
 			goto exit;
 	}
@@ -84,14 +87,34 @@ int rotateRobot(int angle) //rotates robot in place to given angle then stops. P
 	drive(0);
 	return abs(getGyroDegrees(S4));
 }
-
+int rotateAbsolute(int angle) //rotates robot in place to given angle then stops. Positive angles are clockwise when viewed from above
+{
+	const float kP = 0.5;//0.26
+	const float kI = 0.001;//0.0008
+	const float kD = 0.01;//0.23
+	const float tolerance = 0.25;
+	float error = angle - (getGyroDegrees(S4));//
+	float mPower = 0;
+	float prevError = 0;
+	time1[T1] = 0;// reset timer for PID loop
+	while (!getButtonPress(buttonEnter) && abs((getGyroDegrees(S4)) - angle) > tolerance)
+	{
+		error = angle - getGyroDegrees(S4);// error for turn PID
+		mPower = kP*error + kI*((error+prevError)*(time1[T1] + 1)/2) + kD*abs(((error-prevError)/(time1[T1] + 1)));// turn PID calculation
+		driveBoth(-mPower, mPower);// turn motors based on motor power from PID with one being negative
+		prevError = error;// previous error for PID
+		displayString(5, "%f",getGyroDegrees(S4));
+	}
+	drive(0);
+	return abs(getGyroDegrees(S4));
+}
 
 void drivePID(int distance)
 {
 	const float kP = 0.85;
 	const float kI = 0.005;
 	const float kD = 0.05;
-	const float TickToCM = 180/(PI*2.75);
+	const float TickToCM = 180/(PI*1.6);
 	const float tolerance = 0.5;
 	nMotorEncoder[motorA] = 0;
 	float error = distance - nMotorEncoder[motorA]*TickToCM;
@@ -111,15 +134,57 @@ void drivePID(int distance)
 	drive(0);
 }
 
+void correctiveDrive(int distance)
+{
+	//Driving PID Constants
+	const float kP = 1;
+	const float kI = 0.001;
+	const float kD = 0.02;
+	//Turn PID Constants
+	const float turnkP = 0.5;//0.26
+	const float turnkI = 0.0005;//0.0008
+	const float turnkD = 0.01;//0.23
+	float angle = getGyroDegrees(S4);//get angle before driving starts
+	float turnError = 0;
+	float mTurnPower = 0;
+	float turnPrevError = 0;
+	const float TickToCM = 180/(PI*1.6);//Encoder ticks to CM calculation
+	const float tolerance = 0.5;
+	nMotorEncoder[MOTOR_RIGHT] = 0;
+	nMotorEncoder[MOTOR_LEFT] = 0;
+	float error = distance;
+	displayString(5, "%f",error);
+	float mPower = 0;
+	float prevError = 0;
+	time1[T1] = 0;// start timer for PID loop
+	while (!getButtonPress(buttonEnter) && abs(error) > tolerance && SensorValue[S1] == 0)
+	{
+		turnError = angle - getGyroDegrees(S4);// Turn PID error
+		mTurnPower = turnkP*turnError + turnkI*((turnError+turnPrevError)*(time1[T1] + 1)/2) + turnkD*(turnError-turnPrevError);// turn pid calculation
+		error = distance - ((nMotorEncoder[MOTOR_RIGHT] + nMotorEncoder[MOTOR_LEFT])/2)/TickToCM;// Drive PID error
 
+		mPower = kP*error + kI*((error+prevError)*(time1[T1] + 1)/2) + kD*abs(((error-prevError)/(time1[T1] + 1)));// drive PID calculation
+		displayString(7, "%f",mPower);
+		if(mPower > MAX_POWER)
+		{
+			mPower = MAX_POWER;
+		}
+		driveBoth(mPower -mTurnPower, mPower +mTurnPower);//add turn power to drive power to adjust for
+		displayString(5, "%f",getGyroDegrees(S4));// printing Gyro Degrees
+		prevError = error;// for PID
+		turnPrevError = turnError;// for Turn PID
+	}
+	rotateAbsolute(angle);
+	drive(0);// stop motors
+}
 void driveBoth(int pwrL, int pwrR)
 {
-	motor[motorLeft] = pwrL;
-	motor[motorRight] = pwrR;
+	motor[MOTOR_LEFT] = pwrL;
+	motor[MOTOR_RIGHT] = pwrR;
 }
 
 
 void drive(int pwr)
 {
-	motor[motorLeft] = motor[motorRight] = pwr;
- }
+	motor[MOTOR_LEFT] = motor[MOTOR_RIGHT] = pwr;
+}
